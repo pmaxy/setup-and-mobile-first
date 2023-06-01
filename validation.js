e7a1922c4617a86e7b22222c1bc65da432b691eb
pmaxy <84446514+pmaxy@users.noreply.github.com>
2023-06-01 04:29:20 -0600

    add changes

---
+++ validation.js
@@ -0,0 +1,22 @@
+const form=document.getElementsById("form");
+const validationsMsg=document.querySelector('.validation_msg');
+const {name, email, description}= form.elements;
+const err={
+    Email_Lowercase:"Kindly, use lowercase letteres for EmailAddress"
+}
+
+const showMessage=(isError, message)=>{
+    validationsMsg.classList.remove("d-none");
+    validationsMsg.textContent=message;
+};
+
+form.addEventListener('submit', (e)  => {
+    e.preventDefault();
+    if(email.value === email.value.toLowerCase()){
+        showMessage(false,"Complete!");
+        form.submit();
+        return;
+    }
+
+    showMessage(true, err.Email_Lowercase);
+});
